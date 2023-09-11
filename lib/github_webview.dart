95735d077cd7faff63f1eac11b940a050a8619ec
Shammah Sopulu <shammahsopulu0@gmail.com>
2023-09-11 15:51:16 +0100

    Updated webview controller

--- lib/github_webview.dart
+++ lib/github_webview.dart
@@ -8,23 +8,6 @@ class GitHubWebView extends StatelessWidget {
  final controller = WebViewController()
   ..setJavaScriptMode(JavaScriptMode.unrestricted)
   ..setBackgroundColor(const Color(0x00000000))
-  ..setNavigationDelegate(
-    NavigationDelegate(
-      onProgress: (int progress) {
-        // Update loading bar.
-        }
-        ,
-      onPageStarted: (String url) {},
-      onPageFinished: (String url) {},
-      onWebResourceError: (WebResourceError error) {},
-      // onNavigationRequest: (NavigationRequest request) {
-      //   if (request.url.startsWith('https://www.youtube.com/')) {
-      //     return NavigationDecision.prevent;
-      //   }
-      //   return NavigationDecision.navigate;
-      // },
-    ),
-  )
   ..loadRequest(Uri.parse('https://github.com/Void-0x32'));
 
   @override
